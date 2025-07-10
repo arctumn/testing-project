6a0641efb14dee18bad79b1a88f733a9703b124b
Pedro Lopes <pedro.pires.lopes@outlook.com>
2025-07-10 13:05:57 +0100

    remove an unneeded await

--- e2e/components/login/login.spec.ts
+++ e2e/components/login/login.spec.ts
@@ -3,7 +3,7 @@ import {test, expect} from '@playwright/test'
 test('Navigate to register page', async ({page}) =>{
     await page.goto('/login');
     await page.getByRole('button', { name: 'Register' }).click();
-    const registerHeading = await page.getByRole('heading', { name: 'Register' })
+    const registerHeading = page.getByRole('heading', { name: 'Register' })
     const registerValue = await registerHeading.textContent()
     expect(registerValue?.trim()).toBe('Register');
 })
